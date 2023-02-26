eb4fa48f314e4d50c57f72fdbfa77716186fd1d4
isroil <isroilbotiraliyev@email.com>
2023-02-26 21:26:40 +0500

    origin

---
+++ main.js
@@ -0,0 +1,17 @@
+var son = prompt('yoshingizni kiriting');
+
+
+    if (son >= 0 && son <= 17) {
+        alert("Yoshsiz, O'qishingiz kerak");
+    } else if (son >= 18 && son <= 49) {
+        alert("Ishlashingiz kerak");
+    } else if (son >= 50 && son <= 58) {
+        alert("Yaqinda pensyaga chiqasiz");
+    } else if (son >= 59 && son <= 100) {
+        alert("Pensyanega o'xshaysiz, hali tirik bo'lsangiz : ) )");
+    }else { 
+        alert('nimadur xato ketti');
+    }
+
+
+
